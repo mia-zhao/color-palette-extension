562f4a1a870bb55a4de14f83aa82c8b1d8101237
Mia Zhao <miayunyizhao@gmail.com>
2024-02-26 21:27:12 -0800

    use eye dropper

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -2,6 +2,8 @@ console.log("content");
 
 const cursorImageUrl = chrome.runtime.getURL("src/assets/cursor-24.png");
 
+const state = { isColorPicking: false };
+
 function msgListener(
   message: any,
   sender: chrome.runtime.MessageSender,
@@ -17,6 +19,7 @@ function msgListener(
     document.getElementsByTagName(
       "body"
     )[0].style.cursor = `url("${cursorImageUrl}"), default`;
+    state.isColorPicking = true;
     sendResponse({ status: "Ok" });
     return true;
   } else {
@@ -27,6 +30,22 @@ function msgListener(
 
 chrome.runtime.onMessage.addListener(msgListener);
 console.log("listener added");
+
+// Add click event listener to the document
+document.addEventListener("click", function () {
+  if (state.isColorPicking) {
+    console.log("clicked...");
+    console.log("isSecureContext", window.isSecureContext);
+    if (window.EyeDropper != undefined) {
+      const eyeDropper = new window.EyeDropper();
+      (async () => {
+        const res = await eyeDropper?.open();
+        console.log("res", res);
+      })();
+    }
+    state.isColorPicking = false;
+  }
+});
 
 // // update all cursor
 // const styleSheet = `
