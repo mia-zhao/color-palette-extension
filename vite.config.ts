2074647833b74246d4beaeb68d68a0b3a48d840b
Mia Zhao <miayunyizhao@gmail.com>
2024-02-26 19:04:38 -0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json" assert { type: "json" };
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+  },
+});
