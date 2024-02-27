bd5aa6ff855ba7d5de0da8c8f3eb27ac5ccd918e
Mia Zhao <miayunyizhao@gmail.com>
2024-02-26 20:04:25 -0800

    add picker button

---
+++ src/components/ColorPickerButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+function pickColor() {
+  console.log("clicked");
+}
+</script>
+<template>
+  <button type="button" @click="pickColor()">pick a color</button>
+</template>
+
+<style scoped>
+button {
+  background-color: brown;
+}
+</style>
